f6bda1029251985c97c337afa278567f324fabe8
278161009@qq.com <you@example.com>
2019-05-30 19:07:53 +0800

    Update Project更新

---
+++ app/src/main/java/com/hs/hsj/common/Demo.java
@@ -0,0 +1,11 @@
+package com.hs.hsj.common;
+
+/*
+ *  Copyright  2018 Radiance Instruments Ltd. All rights reserved.
+ *  author ArdWang
+ *  email 278161009@qq.com
+ *  Created by ArdWang on 2019/5/30.
+ */
+
+public class Demo {
+}
